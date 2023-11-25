53b76218c7a68c7558d17d77c0843188dbfa92d4
Gabriella Zanin <a62836@ualg.pt>
2023-11-25 15:37:22 +0000

    Updates main.py

--- main.py
+++ main.py
@@ -6,26 +6,6 @@ from io_terminal import pause
 from veiculos import cria_novo_veiculo, imprime_lista_de_veiculos
 
 
-
-####################################################################
-#
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome main-*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro main.py existente, deve apagar
-# todos os outros ficheiros main-*.py, e inclusive estes comentários
-
-# ...
-
-
-
-
-####################################################################
-
-if __name__ == "__main__":
-    menu()
-
-
 def menu():
     """Menu principal da aplicação"""
 
@@ -89,3 +69,8 @@ def menu():
         elif op == "lf":
             imprime_lista_de_faturas(lista_de_faturas)
             pause()
+
+
+if __name__ == "__main__":
+    menu()
+    
