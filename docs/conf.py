f7fc61101b2018fa7775128f861bda21bf39ac8b
gabriellazanin <149081568+gabriellazanin@users.noreply.github.com>
2023-11-26 23:44:33 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,12 +9,6 @@ import sys
 sys.path.insert(0, os.path.abspath(r'C:\Users\gabi-\OneDrive\Documentos\adc\base\src\Trabalho'))
 
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-import os
-import sys
-
 project = 'Trabalho Oficina'
 copyright = '2023, Grupo C'
 author = 'Gabriella Zanin, David Valente, Sofia Martins'
