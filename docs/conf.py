90283543f1911f5bdbeb618370fbb6d8c86517bc
Davydz4 <149081611+Davydz4@users.noreply.github.com>
2023-11-26 22:52:13 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,6 @@
 import os
 import sys
 
-sys.path.insert(0, '/home/gabriellazanin/ualg/adc/base/Trabalho_Oficina')
-
 project = 'Trabalho Oficina'
 copyright = '2023, Grupo C'
 author = 'Gabriella Zanin, David Valente, Sofia Martins'
